88ee1cfe4048803e28becdee091074a93faeea03
marius <marnyg@proton.me>
2022-08-07 20:12:22 +0200

    nvim

--- config/homemanager/programs/nvim.nix
+++ config/homemanager/programs/nvim.nix
@@ -1,20 +1,20 @@
 { pkgs, config, ... }:
 {
-  # home.file = {
-  #   ".config/nvim".source = pkgs.fetchFromGitHub {
-  #     owner = "marnyg";
-  #     repo = "nvim-conf";
-  #     rev = "test";
-  #     # rev = "8861e72";
-  #     # sha256 = "jZixvObvw2WX+qigV+GKe4z+UlhM8T3kJvrV9E8qE6w=";
-  #     sha256 = "4R2X30GuERrGdjH7kg4d3MXmYaiK40G+zkAkbKZkf2E=";
-  #   };
-  # };
-
-
   home.file = {
-    ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/mar/git/nvim-conf";
+    ".config/nvim".source = pkgs.fetchFromGitHub {
+      owner = "marnyg";
+      repo = "nvim-conf";
+      rev = "test";
+      # rev = "8861e72";
+      # sha256 = "jZixvObvw2WX+qigV+GKe4z+UlhM8T3kJvrV9E8qE6w=";
+      sha256 = "5h3mtocJiU04MjEVmgYk10Feb4GbOsoSubQWCbwfkFo=";
+    };
   };
+
+
+  # home.file = {
+  #   ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/mar/git/nvim-conf";
+  # };
 
 
 
