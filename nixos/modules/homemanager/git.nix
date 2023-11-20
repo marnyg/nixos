f62e60997427dc38ce190e0f00b25dcb9c24eaab
marius <marnyg@proton.me>
2023-11-20 14:17:37 +0000

    improving git config

--- nixos/modules/homemanager/git.nix
+++ nixos/modules/homemanager/git.nix
@@ -35,8 +35,12 @@ with lib;
           "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
       };
       extraConfig = {
-        init = { defaultBranch = "main"; };
+        init = { 
+          defaultBranch = "main"; 
+        };
+        push.autoSetupRemote= true;
         core.editor = "nvim";
+        pull.rebase = true;
       };
     };
   };
