02a422d09c6b2c6d593aba0b2d083980ef4ba267
marius <marnyg@proton.me>
2023-12-04 11:30:28 +0000

    fix

--- nixos/modules/homemanager/git.nix
+++ nixos/modules/homemanager/git.nix
@@ -29,6 +29,7 @@ with lib;
         a = "add";
         ai = "add -i";
         s = "status";
+        undo = "reset HEAD~";
         lg =
           "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
         lg2 =
