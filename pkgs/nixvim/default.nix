113ab8cabd745b4f59fe33aa4b3e03b37ed92837
marius <marnyg@proton.me>
2024-08-06 12:23:52 +0000

    fix: fix

--- pkgs/nixvim/default.nix
+++ pkgs/nixvim/default.nix
@@ -19,30 +19,9 @@ in
   flake.nixvimModules = {
     nixVim = nixvimModule;
   };
-  flake.HomemanagerModules.nixvim = { config, pkgs, ... }: {
-    home-manager.users.juuso.programs.nixvim =
-      let neovim = (import ./nixvim.nix) { inherit config pkgs; };
-      in with neovim.config; {
-        inherit colorschemes extraConfigVim extraConfigLua extraPackages plugins extraPlugins;
-        enable = true;
-        viAlias = true;
-        vimAlias = true;
-        defaultEditor = true;
-      };
-    home-manager.users.juuso.editorconfig = {
-      enable = true;
-      settings = {
-        "*" = {
-          charset = "utf-8";
-          end_of_line = "lf";
-          trim_trailing_whitespace = true;
-          insert_final_newline = false;
-          max_line_width = 78;
-          indent_style = "space";
-          indent_size = 2;
-        };
-      };
-    };
+  flake.HomemanagerModules.nixvim = { pkgs, ... }: {
+    programs.nixvim.enable = true;
+    programs.nixvim.pkgs = self.packages.${pkgs.system}.nixvim;
   };
 
   flake.nixosModules =
@@ -60,12 +39,12 @@ in
             # tmp fix for broken neorg, see: 
             #  https://github.com/NixOS/nixpkgs/pull/302442
             #  https://github.com/nix-community/nixvim/issues/1395
-#            _module.args.pkgs = import inputs.nixpkgs {
-#              system = "x86_64-linux";
-#              overlays = [
-#                inputs.neorg-overlay.overlays.default
-#              ];
-#            };
+            #            _module.args.pkgs = import inputs.nixpkgs {
+            #              system = "x86_64-linux";
+            #              overlays = [
+            #                inputs.neorg-overlay.overlays.default
+            #              ];
+            #            };
             # end tmp
             environment.systemPackages = [ self.packages.${pkgs.system}.nixvim ];
 
