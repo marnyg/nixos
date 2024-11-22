3b1448fa256d8b2d7a8b1a4b01f104d7b2b71dcb
marius <marnyg@proton.me>
2024-11-22 13:35:35 +0000

    feat: avante

--- pkgs/nixvim/nixvim.nix
+++ pkgs/nixvim/nixvim.nix
@@ -309,41 +309,41 @@
       virtual-types-nvim
 
       #installing parrot.nvim from https://github.com/frankroeder/parrot.nvim?tab=readme-ov-file#roadmap
-      {
-        plugin = (pkgs.fetchFromGitHub
-          {
-            owner = "frankroeder";
-            repo = "parrot.nvim";
-            rev = "976c37462436ada5ea2239430a1e5bb2ce52944a";
-            sha256 = "sha256-AF7nM/sWONhS7vDwbvOxEYHSwrRndaC1GvS5MlXBIts=";
-          });
-        config = /*lua*/''
-          lua <<EOF
-            require("parrot").setup {
-              providers = {
-                anthropic = {
-                  api_key = os.getenv "ANTHROPIC_API_KEY",
-                },
-              },
-              -- Default target for  PrtChatToggle, PrtChatNew, PrtContext and the chats opened from the ChatFinder
-              -- values: popup / split / vsplit / tabnew
-              toggle_target = "popup",
-            }
+      #{
+      #  plugin = (pkgs.fetchFromGitHub
+      #    {
+      #      owner = "frankroeder";
+      #      repo = "parrot.nvim";
+      #      rev = "976c37462436ada5ea2239430a1e5bb2ce52944a";
+      #      sha256 = "sha256-AF7nM/sWONhS7vDwbvOxEYHSwrRndaC1GvS5MlXBIts=";
+      #    });
+      #  config = /*lua*/''
+      #    lua <<EOF
+      #      require("parrot").setup {
+      #        providers = {
+      #          anthropic = {
+      #            api_key = os.getenv "ANTHROPIC_API_KEY",
 
-            --keymaps
-            vim.g.mapleader = ' '
-            vim.g.maplocalleader = ' '
-            vim.api.nvim_set_keymap('n', '<leader>ac', '<cmd>PrtChatToggle<CR>', { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('v', '<leader>ac', ":PrtChatPaste<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>an', '<cmd>PrtChatNew popup<CR>', { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>af', '<cmd>PrtChatFinder<CR>', { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('v', '<leader>ar', ":'<,'>PrtRewrite<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('v', '<leader>aa', ":'<,'>PrtAppend<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('v', '<leader>ap', ":'<,'>PrtPrepend<CR>", { noremap = true, silent = true })
-          EOF
-        '';
+      #        },
+      #        -- Default target for  PrtChatToggle, PrtChatNew, PrtContext and the chats opened from the ChatFinder
+      #        -- values: popup / split / vsplit / tabnew
+      #        toggle_target = "popup",
+      #      }
 
-      }
+      #      --keymaps
+      #      vim.g.mapleader = ' '
+      #      vim.g.maplocalleader = ' '
+      #      vim.api.nvim_set_keymap('n', '<leader>ac', '<cmd>PrtChatToggle<CR>', { noremap = true, silent = true })
+      #      vim.api.nvim_set_keymap('v', '<leader>ac', ":PrtChatPaste<CR>", { noremap = true, silent = true })
+      #      vim.api.nvim_set_keymap('n', '<leader>an', '<cmd>PrtChatNew popup<CR>', { noremap = true, silent = true })
+      #      vim.api.nvim_set_keymap('n', '<leader>af', '<cmd>PrtChatFinder<CR>', { noremap = true, silent = true })
+      #      vim.api.nvim_set_keymap('v', '<leader>ar', ":'<,'>PrtRewrite<CR>", { noremap = true, silent = true })
+      #      vim.api.nvim_set_keymap('v', '<leader>aa', ":'<,'>PrtAppend<CR>", { noremap = true, silent = true })
+      #      vim.api.nvim_set_keymap('v', '<leader>ap', ":'<,'>PrtPrepend<CR>", { noremap = true, silent = true })
+      #    EOF
+      #  '';
+
+      #}
 
       vim-dadbod-ui # TODO: add keybindings for opening dbui
       # TODO: add https://github.com/chrisgrieser/nvim-various-textobjs
